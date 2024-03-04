e9083b9220e27e814a9bb2d9a98af224a58e0fdd
JoeArias1121 <joearias1121@gmail.com>
2024-03-04 09:19:06 -0500

    used vite to quick template

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+//import { RouterView } from 'vue-router'
+import home from  './pages/home.vue'
+import navBar from  "./components/navBar.vue"
+</script>
+
+<template>
+    <div>
+        <nav-bar />
+        <home />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ client/src/pages/home.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from "vue"
+const counter = ref(0)
+
+const increment = () => {
+    counter.value++
+}
+</script>
+
+<template>
+    <div class="container">
+        <button class="button" @click="increment">{{ counter }}</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
